21fac25e7da5c5ad16c7b453ff5de5ebcd5bb5e7
Gulshan Singh <gulshan@umich.edu>
2013-07-28 11:58:46 -0700

    Added extension dropdown list

--- options.js
+++ options.js
@@ -8,8 +8,29 @@ $(document).ready(function() {
 		document.getElementById('urls').value = text;
 	});
 
+	chrome.management.getAll(function(extensions) {
+		var list = document.getElementById('extension-list');
+		for (var i = 0; i < extensions.length; i++) {
+			if (extensions[i].installType == "development") {
+				var li = document.createElement('li');
+				li.innerHTML = extensions[i].name;
+				list.appendChild(li);
+			}
+		}
+	});
+
 	document.getElementById('save').addEventListener("click", function() {
 		urls = document.getElementById('urls').value.split('\n');
 		chrome.storage.local.set({"urls": urls});
 	});
+
+	$('#extension-title').click(function() {
+		var src = $('#triangle').attr('src')
+		if (src == "down_triangle.png")
+			src = "right_triangle.png";
+		else
+			src = "down_triangle.png";
+		$('#triangle').attr('src', src);
+		$('#extension-list').slideToggle();
+	})
 });
